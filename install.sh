556f41bf63d22c296cd2db119be1a444a9a8a925
fxstein <oliver@ratzesberger.com>
2016-04-10 16:21:57 -0700

    Fix syntax error

--- install.sh
+++ install.sh
@@ -105,7 +105,7 @@ fi
 log "Creating SentientHome dir: $SENTIENTHOME_PREFIX/SentientHome ..."
 mkdir -p "$SENTIENTHOME_PREFIX"/SentientHome &>$OPT_OUTPUT
 
-$( cd "$SENTIENTHOME_PREFIX"/SentientHome && git status )
+( cd "$SENTIENTHOME_PREFIX"/SentientHome && git status )
 if [ $? -eq 0 ]; then
   log "SentientHome already installed - Updating ..."
   git pull "$SENTIENTHOME_PREFIX"/SentientHome
